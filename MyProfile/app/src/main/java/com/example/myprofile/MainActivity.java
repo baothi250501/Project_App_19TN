3fee95b468ba278df5c572afb4a448f647bade91
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-10-14 00:03:03 +0700

    Commit Profile Orientation version 2

--- MyProfile/app/src/main/java/com/example/myprofile/MainActivity.java
+++ MyProfile/app/src/main/java/com/example/myprofile/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.myprofile;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+    Bundle newBundle = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditText = findViewById(R.id.editText);
+        if (savedInstanceState != null)
+            onRestoreInstanceState(newBundle);
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        onRestoreInstanceState(newBundle);
     }
 
     @Override
@@ -24,4 +41,24 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onConfigurationChanged(newConfig);
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString("editText",String.valueOf(mEditText.getText()));
+        newBundle.putString("editText",String.valueOf(mEditText.getText()));
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState (Bundle mySavedState) {
+
+        super.onRestoreInstanceState(mySavedState);
+        if (mySavedState != null) {
+            String editText = mySavedState.getString("editText");
+            mEditText = findViewById(R.id.editText);
+            mEditText.setText(editText);
+        }
+
+    }
+
 }
