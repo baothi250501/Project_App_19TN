4024a826a2530e3a11fa3b90bec0e978500a0511
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-10-16 22:21:44 +0700

    Update Alarm Clock Intent Demo

--- DemoIntents/app/src/main/java/com/example/demointents/AlarmClockDemoActivity.java
+++ DemoIntents/app/src/main/java/com/example/demointents/AlarmClockDemoActivity.java
@@ -1,28 +1,27 @@
 package com.example.demointents;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.AlarmClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 
 public class AlarmClockDemoActivity extends AppCompatActivity {
 
-    EditText hourEditText, minuteEditText, messageEditText;
-    Button setTimeButton, setAlarmButton;
-    TimePickerDialog timePickerDialog;
-    Calendar calendar;
+    private EditText hourEditText, minuteEditText, messageEditText;
     int currentHour, currentMinute;
 
-    private TimePickerDialog.OnTimeSetListener timePickerDialogHelper = new TimePickerDialog.OnTimeSetListener(){
+    private final TimePickerDialog.OnTimeSetListener timePickerDialogHelper = new TimePickerDialog.OnTimeSetListener(){
+        @SuppressLint("DefaultLocale")
         @Override
         public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
             hourEditText.setText(String.format("%02d", hourOfDay));
@@ -30,21 +29,22 @@ public class AlarmClockDemoActivity extends AppCompatActivity {
         }
     };
 
-    private View.OnClickListener setTimeHelper = new View.OnClickListener() {
+    private final View.OnClickListener setTimeHelper = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            calendar = Calendar.getInstance();
+            Calendar calendar = Calendar.getInstance();
             currentHour = calendar.get(Calendar.HOUR_OF_DAY);
             currentMinute = calendar.get(Calendar.MINUTE);
 
-            timePickerDialog = new TimePickerDialog(AlarmClockDemoActivity.this, timePickerDialogHelper, currentHour, currentMinute, false);
+            TimePickerDialog timePickerDialog = new TimePickerDialog(AlarmClockDemoActivity.this, timePickerDialogHelper, currentHour, currentMinute, false);
 
             timePickerDialog.show();
         }
     };
 
-    private View.OnClickListener setAlarmHelper = new View.OnClickListener() {
+    private final View.OnClickListener setAlarmHelper = new View.OnClickListener() {
 
+        @SuppressLint({"SetTextI18n", "QueryPermissionsNeeded"})
         @Override
         public void onClick(View view) {
             if (messageEditText.getText().toString().isEmpty()){
@@ -58,7 +58,7 @@ public class AlarmClockDemoActivity extends AppCompatActivity {
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 } else {
-                    Log.e("MainActivity", "No Alarm Clock");
+                    Toast.makeText(AlarmClockDemoActivity.this, "No support Alarm Clock Intent",Toast.LENGTH_SHORT).show();
                 }
             }
         }
@@ -69,11 +69,11 @@ public class AlarmClockDemoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_clock_demo);
 
-        hourEditText = (EditText) findViewById(R.id.hhEditText);
-        minuteEditText = (EditText) findViewById(R.id.mmEditText);
-        messageEditText= (EditText) findViewById(R.id.msgEditText);
-        setTimeButton = (Button) findViewById(R.id.setTimeButton);
-        setAlarmButton =  (Button) findViewById(R.id.setAlarmButton);
+        hourEditText = findViewById(R.id.hhEditText);
+        minuteEditText = findViewById(R.id.mmEditText);
+        messageEditText= findViewById(R.id.msgEditText);
+        Button setTimeButton = findViewById(R.id.setTimeButton);
+        Button setAlarmButton = findViewById(R.id.setAlarmButton);
 
         setTimeButton.setOnClickListener(setTimeHelper);
         setAlarmButton.setOnClickListener(setAlarmHelper);
